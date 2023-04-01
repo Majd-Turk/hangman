12df5ccba93d1df553dd88bfe79ab204f2466ac3
Majd-Turk <123835493+Majd-Turk@users.noreply.github.com>
2023-04-01 20:18:07 +0300

    Add files via upload

---
+++ Solution.js
@@ -0,0 +1,23 @@
+import Letter from "./letter ";
+
+export default function Solution(props) {
+
+    const wordLettersArr = props.solution.word.split('')
+
+    const letterClicked = () => alert('No peeking')
+
+    const displayLetter = function(letter){
+        if(props.letterStatus[letter]){
+            return <Letter letter={letter} key={letter} className={'word'} selectLetter={letterClicked}/>
+        }else{
+            return <Letter letter={'_'} className={'word'} key={`_${letter}`} selectLetter={letterClicked}/>
+        }
+    }
+
+    return (
+        <div>
+            <div> Word : {wordLettersArr.map(letter => displayLetter(letter))}</div>
+            <div> Hint : {props.solution.hint}  </div> 
+        </div>
+    );
+  }
